c9b5b71be942bbf7dac9d845db98f1a3e718cc04
Yangseungchan <uvzone@naver.com>
2020-03-27 19:10:51 +0900

    Update Main function

--- Stacks/FormulaConvertor/FormulaConvertorMain.c
+++ Stacks/FormulaConvertor/FormulaConvertorMain.c
@@ -6,12 +6,15 @@ int main(void)
 {
     char formula[1000];
     int i, length;
+    printf("<Convertor from infix to postfix>\n");
+    printf("Input mathematical expression without space[+, -, *, /, (, ) are possible] : ");
     scanf("%s", formula);
 
     length = strlen(formula);
 
     convertFormula(formula, length);
-    printf("%s\n", formula);
+    putchar('\n');
+    printf("result : %s\n", formula);
 
     return 0;
 }
