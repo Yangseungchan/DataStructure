290585d23bb7c9bd11974cc21e604f04fc745838
Yangseungchan <uvzone@naver.com>
2020-05-12 20:08:35 +0900

    Update and complete Chainhash.h

--- Hash/ChainHash/Chainhash.h
+++ Hash/ChainHash/Chainhash.h
@@ -15,14 +15,20 @@ typedef struct __chainhash
   Node **ChainNode; /* Node Pointer Array; Each element contains Node Pointer that's why it is set as double pointer */
 } ChainHash;
 
-void Initialize(ChainHash *chain, int size);
+void Initialize(ChainHash *chain, int size); /* function that initializes the hashtable including allocation, setting size of table */
 
-Node *SetNodeInfo(Member mem, Node *next);
+Node *SetNodeInfo(Member mem, Node *next); /* function that sets the information of the Node */
 
-int HashFunc(int no, ChainHash *chain);
+int HashFunc(char *name, ChainHash *chain); /* function that gives the hash key using name of member */
 
-int Add(ChainHash *chain, Member data);
+int Add(ChainHash *chain, Member data); /* function that adds the new node to hash table */
 
-int Delete(ChainHash *chain, Member data);
+int Delete(ChainHash *chain, char *name); /* function that deletes the node in hashtable */
 
-#endif __CHAINHASH_H__
+int Search(ChainHash *chain, char *name); /* function that finds the node in hashtable using name or no; sw is MEMBER_NAME or MEMBER_NO */
+
+void Terminate(ChainHash *chain);
+
+void PrintHash(ChainHash *chain); /* function that prints the all nodes in hashtable in order of the keys */
+
+#endif
