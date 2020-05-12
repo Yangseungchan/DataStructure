f4efa2ca0aa47a2eb58aedb048424470180d0c40
Yangseungchan <uvzone@naver.com>
2020-05-12 20:08:24 +0900

    Update and complete Chainhash.c

--- Hash/ChainHash/Chainhash.c
+++ Hash/ChainHash/Chainhash.c
@@ -14,9 +14,15 @@ void Initialize(ChainHash *chain, int size)
   chain->size = size;
 }
 
-int HashFunc(int no, ChainHash *chain)
+int HashFunc(char *name, ChainHash *chain)
 {
-  return no % chain->size;
+  int sum = 0;
+  int len = strlen(name);
+  for (int i = 0; i < len; i++)
+  {
+    sum += (int)name[i];
+  }
+  return sum % chain->size;
 }
 
 Node *SetNodeInfo(Member data, Node *next)
@@ -35,36 +41,118 @@ Node *SetNodeInfo(Member data, Node *next)
 
 int Add(ChainHash *chain, Member data)
 {
-  int key = HashFunc(data.no, chain);
+  int key = HashFunc(data.name, chain);
   Node *tmp;
-  Node *nde = SetNodeInfo(data, NULL); /* Those jobs are done by this func : allocation, setting node's values, setting next pointer */
+  Node *new_nde = SetNodeInfo(data, NULL); /* Those jobs are done by this func : allocation, setting node's values, setting next pointer */
 
   if (!chain->ChainNode[key])
   {
-    chain->ChainNode[key] = nde;
+    chain->ChainNode[key] = new_nde;
   }
   else
   {
     tmp = chain->ChainNode[key];
     do
     {
-      if (cmpMemberName(&tmp->member, &data) == 0 || cmpMemberNo(&tmp->member, &data) == 0) /* When both two member's name and no are same */
+      if (cmpMemberName(&tmp->member, &data) == 0 || cmpMemberNo(&tmp->member, &data) == 0) /* When two member's name or no are same */
       {
         return -1;
       }
       if (!(tmp->next))
-      { /*this roops are repeated until the next node is NULL */
+      { /*this roop is repeated until the next node is NULL */
         break;
       }
       tmp = tmp->next;
     } while (1);
+    tmp->next = new_nde; /* connecting the node in the assigned key with new added node */
+  }
+  return 0;
+}
+
+int Delete(ChainHash *chain, char *name) /* function that deletes the node which has same name with input in hashtable */
+{
+  int key = HashFunc(name, chain);
+  Node *tmp = chain->ChainNode[key];
+  Node *prev = NULL;
+  while (tmp)
+  {
+    if (!strcmp(tmp->member.name, name))
+    {
+      if (prev)
+      { /* when prev is not NULL */
+        prev->next = tmp->next;
+      }
+      else
+      { /* when prev is NULL = tmp is just the node that is pointed by chain->ChainNode[key] */
+        chain->ChainNode[key] = tmp->next;
+      }
+      tmp->next = NULL;
+      free(tmp);
+      return 0;
+    }
+    prev = tmp;
+    tmp = tmp->next;
+  }
+  return -1;
+}
+
+int Search(ChainHash *chain, char *name) /* function that finds the node in hashtable using name */
+{
+  int key = HashFunc(name, chain);
+  Node *tmp = chain->ChainNode[key];
+  while (tmp)
+  {
+    if (!strcmp(tmp->member.name, name))
+    {
+      return key;
+    }
+    tmp = tmp->next;
+  }
+  return -1;
+}
+
+void Terminate(ChainHash *chain)
+{ /* function that deallocates the hashtable */
+  Node *ptr;
+  Node *next;
+  for (int i = 0; i < chain->size; i++)
+  { /* On each key index */
+    ptr = chain->ChainNode[i];
+    while (ptr)
+    { /* disallocation of each node is done until all of them are disallocated*/
+      next = ptr->next;
+      ptr->next = NULL;
+      free(ptr);
+      ptr = next;
+    }
+    free(chain->ChainNode[i]);
   }
 }
 
-int Delete(ChainHash *chain, Member data);
-
-int main(void)
-{ /*main function for test */
-
-  return 0;
+void PrintHash(ChainHash *chain)
+{
+  int i, size;
+  size = chain->size;
+  Node *ptr;
+  printf("====================<Printing HashTable>====================\n");
+  for (i = 0; i < size; i++)
+  {
+    printf("    [%2d] -> ", i);
+    ptr = chain->ChainNode[i];
+    if (!ptr)
+    { /* when ptr points nothing(NULL) */
+      printf("[NULL]\n");
+    }
+    else
+    {
+      while (ptr->next)
+      {
+        PrintMember(&ptr->member);
+        printf(" -> ");
+        ptr = ptr->next;
+      }
+      PrintMemberln(&ptr->member);
+    }
+  }
+  printf("=============================================================\n\n\n");
 }
