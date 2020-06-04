4b5bab1a42d4854420e0d8e4061f2e240d0fd59e
Yangseungchan <uvzone@naver.com>
2020-06-04 23:17:45 +0900

    Update header file of Heap

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -3,19 +3,20 @@
 
 #define MAXNUM 100
 
+typedef char HData;
+typedef int Priority;
 
-typedef char HData
-typedef int Priority
+typedef struct __heapnode
+{
+    HData data;  /* data */
+    Priority pr; /* node's priority; the lower pr is, the higher the real priority is */
+} HNode;
 
-typedef struct __heapnode{
-  HData data;   /* data */
-  Priority pr;  /* node's priority; the lower pr is, the higher the real priority is */
-}HNode;
-
-typedef struct __heap{
-  int numofData;    /* number of datas that is contained by the Heap array = index of last node */
-  HNode HNArr[MAXNUM]; /* array that contains HNode; its index is started from 1; 0 should be empty */
-}Heap;
+typedef struct __heap
+{
+    int numofData;       /* number of datas that is contained by the Heap array = index of last node */
+    HNode HNArr[MAXNUM]; /* array that contains HNode; its index is started from 1; 0 should be empty */
+} Heap;
 
 void InitHeap(Heap *hp); /* function that initializes the Heap */
 
@@ -25,6 +26,10 @@ int GetLChildIdx(int idx); /* function that returns the left child's index using
 
 int GetRChildIdx(int idx); /* function that returns the right child's index using given idx */
 
-int GetHighPrChIdx(Heap *hp, Priority pr, HData, data); /* function that returns child node index which has high priority; this function is used to delete a node */
+int GetHighPrChIdx(Heap *hp, int idx); /* function that returns child node index which has high priority; this function is used to delete a node */
+
+int HInsert(Heap *hp, HData data, Priority pr); /* function that inserts new data into heap */
+
+int HDelete(Heap *hp); /* function that deletes a node which has the highest priority */
 
 #endif
